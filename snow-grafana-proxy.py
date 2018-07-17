7835eb6e1187392b148b525623c802c708c4aeae
marcin <marcin@localhost.localdomain>
2018-07-17 21:43:24 +0200

    New generalized version much more configurable in YAML format

--- snow-grafana-proxy.py
+++ snow-grafana-proxy.py
@@ -9,15 +9,17 @@ import calendar
 from BaseHTTPServer import BaseHTTPRequestHandler,HTTPServer
 import logging
 
-def MakeSnowRequestHandler(url,snowAuth,snowFilter):
+def MakeSnowRequestHandler(snowParams):
 	class SnowRequestsHandler(BaseHTTPRequestHandler,object):
 		knownUsers={}
-		lastQueryReply=[0,[]]
+		lastQueryReply={}
 	        
 		def __init__(self,*args,**kwargs): #url, snowAuth,snowFilter):
-			self.snowUrl=url	
-			self.snowAuth=snowAuth
-			self.snowFilter=snowFilter
+			self.snowUrl=snowParams["instance"]["url"]	
+			self.snowAuth=(snowParams["instance"]["user"],snowParams["instance"]["password"])
+			self.queries=snowParams["queries"]
+			for k,v in self.queries.iteritems():
+				self.lastQueryReply[k]=dict(time=0)
 			super(SnowRequestsHandler, self).__init__(*args,**kwargs)
 
 	
@@ -35,6 +37,21 @@ def MakeSnowRequestHandler(url,snowAuth,snowFilter):
 			
 			person=r.json()
 			return person["result"]
+		def __get_row(self,attributes,dataRow):
+			resultRow=[]
+			for attribute in attributes:
+				if attribute["interpreter"] == "none":
+					toAdd=dataRow[attribute["name"]]
+				elif attribute["interpreter"] == "person_by_link":
+					toAdd=self._get_person_by_link(dataRow[attribute["name"]]["link"])
+				elif attribute["interpreter"] == "map":
+					toAdd=attribute["map"][dataRow[attribute["name"]]]
+				else:
+					raise ValueError('Unsupported interpreter '+str(attribute["interpreter"]))
+				resultRow.append(toAdd)
+			return resultRow
+				
+				
 
 		def do_GET(self):
 			logging.info("GET"+self.path)
@@ -59,12 +76,17 @@ def MakeSnowRequestHandler(url,snowAuth,snowFilter):
 				except ValueError:
 					logging.warning("Received incorrect json")	
 			elif self.path=="/search":
-
-				response=["SELECT incident_number,assigned_to_last_name FROM incidents where incident_state not in 6,7,8"]
+				response=[]
+				for k,v in self.queries.iteritems():
+					response.append(k)
 				self._set_headers()
 				self.wfile.write(json.dumps(response));
+				logging.debug("Service reply is:"+json.dumps(response))
 
 			elif self.path=='/query':
+				target_name=received["targets"][0]["target"]
+				target=self.queries[target_name]
+				logging.debug("My query target is:"+str(target))
 
 				incidents_description= { "1": "New" , "2": "Assigned", "3": "In progress", "12": "Referred", "4": "Await User", "5": "Await Evidance", "10": "Await Change", "8": "Await Vendor", "11": "Await Vendor Change", "6": "Resolved", "7": "Closed"}
 				self._set_headers()
@@ -72,50 +94,59 @@ def MakeSnowRequestHandler(url,snowAuth,snowFilter):
 				
 				now=calendar.timegm(time.gmtime())
 				queryReply=[{}]
-				if (now - self.lastQueryReply[0] > 60):
-					url = 'https://delphi.service-now.com//api/now/table/incident' 
-					#data = (("assignment_group","10a440771322a2c0929930ded144b022"),("incident_state", "2"))
+				if (now - self.lastQueryReply[target_name]["time"] > target["cacheTime"]):
 
 #Do request to service-now
 					snow = requests.Session()
 					snow.headers.update({"Accept": "application/json" })
 					snow.auth=self.snowAuth
-					logging.debug("My snow filter is:"+self.snowFilter)
+					logging.debug("My snow filter is:"+target["snowFilter"])
 
 					snow.verify=False
-					r=snow.get(self.snowUrl+"//api/now/table/incident",params=self.snowFilter)
+					r=snow.get(self.snowUrl+"//api/now/table/incident",params=target["snowFilter"])
 
-					incidents=r.json()
+
+					if r.status_code == 401:
+						logging.error("Unauthorized request to service-now instance - check user and password")
+						return
+					elif r.text == "":
+						logging.warning("Empty reply from service-now instance")
+						return
+				
+					print r.text	
+					items=r.json()
 
 		
-					
-					logging.debug("Results from service-now in json format:"+json.dumps(incidents["result"],indent=4,sort_keys=True))
+					logging.debug("Results from service-now in json format:"+json.dumps(items,indent=4,sort_keys=True))
+
 					#queryReply[0]["columns"]=[{"text": "Number", "type": "string"}, {"text": "Short description", "type": "string"},{"text": "Last update by", "type": "string"}]
 					queryReply[0]["columns"]=[{"text": "Number", "type": "string"}, {"text": "Assigned to", "type": "string"}, { "text": "Incident state", "type": "string"}]
 					queryReply[0]["rows"]=[]
-					for incident in incidents["result"]:
-						try:			
-						
-							if(incident["assigned_to"]==""):
-								queryReply[0]["rows"].append([ incident["number"], "unassigned" , incidents_description[incident["incident_state"]]] )
-							else:
-								queryReply[0]["rows"].append([ incident["number"],  self.knownUsers[incident["assigned_to"]["value"]] , incidents_description[incident["incident_state"]]])
-
-						except KeyError:
-							try:
-								self.knownUsers[incident["assigned_to"]["value"]]=self._get_person_by_link(incident["assigned_to"]["link"])["last_name"]
-								logging.info("User"+self.knownUsers[incident["assigned_to"]["value"]]+" added to my cache")
-								queryReply[0]["rows"].append([ incident["number"],  self.knownUsers[incident["assigned_to"]["value"]], incidents_description[incident["incident_state"]]])
-							except:
-								queryReply[0]["rows"].append([ incident["number"],  "NN", incidents_description[incident["incident_state"]]])
-								logging.error("Unable to get user last_name, I've got:",self._get_person_by_link(incident["assigned_to"]["link"]))
-						
+					for row in items["result"]:
+						oneResultRow=self.__get_row(target["attributes"],row)
+						queryReply[0]["rows"].append(oneResultRow)
+#						try:			
+#						
+#							if(incident["assigned_to"]==""):
+#								queryReply[0]["rows"].append([ incident["number"], "unassigned" , incidents_description[incident["incident_state"]]] )
+#							else:
+#								queryReply[0]["rows"].append([ incident["number"],  self.knownUsers[incident["assigned_to"]["value"]] , incidents_description[incident["incident_state"]]])
+#
+#						except KeyError:
+#							try:
+#								self.knownUsers[incident["assigned_to"]["value"]]=self._get_person_by_link(incident["assigned_to"]["link"])["last_name"]
+#								logging.info("User"+self.knownUsers[incident["assigned_to"]["value"]]+" added to my cache")
+#								queryReply[0]["rows"].append([ incident["number"],  self.knownUsers[incident["assigned_to"]["value"]], incidents_description[incident["incident_state"]]])
+#							except:
+#								queryReply[0]["rows"].append([ incident["number"],  "NN", incidents_description[incident["incident_state"]]])
+#								logging.error("Unable to get user last_name, I've got:",self._get_person_by_link(incident["assigned_to"]["link"]))
+#						
 					queryReply[0]["type"]="table"
-					self.lastQueryReply[1]=queryReply
-					self.lastQueryReply[0]=now
+					self.lastQueryReply[target_name]["reply"]=queryReply
+					self.lastQueryReply[target_name]["time"]=now
 				else:
-					queryReply=self.lastQueryReply[1]
-					cacheAge=now - self.lastQueryReply[0]
+					queryReply=self.lastQueryReply[target_name]["reply"]
+					cacheAge=now - self.lastQueryReply[target_name]["time"]
 					logging.info("Reply from cache - "+str(cacheAge)+" seconds old")
 				queryReply=json.dumps(queryReply)
 				logging.debug("Reply to grafana:"+queryReply)
@@ -135,14 +166,26 @@ def run( handler_class, server_class=HTTPServer,port=8095,address='127.0.0.1'):
 
 
 if __name__ == "__main__":
-	import configparser
-	config=configparser.ConfigParser()
-	config.read('/etc/snow-grafana-proxy.conf')
+	
+	import argparse
+	parser = argparse.ArgumentParser(description='Simple JSON data source for grafana retreving data from service-now')
+	parser.add_argument('-c', '--configFile',help='Configuration file to use', dest='configFile', default='/etc/snow-grafana-proxy.conf')
+	parser.add_argument('-d', '--debug',help='Start in foreground and print all logs to stdout', action='store_true',dest='debugMode')
+	
+	args = parser.parse_args()
+	
+	import yaml,sys
+	with open(args.configFile) as configStream:
+		config=yaml.load(configStream)
+
 	port=int(config['service']['port'])
-	logging.basicConfig(filename=config['service']['logfile'],level=getattr(logging,config['service']['loglevel'].upper()))
+	if args.debugMode==True:
+		logging.basicConfig(stream=sys.stdout,level=logging.DEBUG)
+	else:
+		logging.basicConfig(filename=config['service']['logfile'],level=getattr(logging,config['service']['loglevel'].upper()))
 	logging.info("My loglevel is:"+config['service']['loglevel'])
 	
-	run(port=int(config['service']['port']),address=config['service']['address'],handler_class=MakeSnowRequestHandler(url=config['service-now']['url'],snowAuth=(config['service-now']['user'],config['service-now']['password']),snowFilter=config['service-now']['filter']) )
+	run(port=int(config['service']['port']),address=config['service']['address'],handler_class=MakeSnowRequestHandler(config['service-now']))
 	logging.shutdown()
 
 
